f0a451bf9eb31578d507e57bc9cc88b64b0e5329
tkrausjr <tkrausjr@gmail.com>
2017-05-02 16:52:49 -0400

    Added configurable ZK Node Content

--- zk_tests.py
+++ zk_tests.py
@@ -6,20 +6,21 @@ import socket
 
 zk_hosts = '54.193.113.160:2181,54.215.214.63:2181,52.53.184.163:2181'
 zk_port = 2181
-zk_root_path = "/a-host"
-zk_node_name = "test-node"
+zk_root_path = "/b-host"
+zk_node_name = "b-node"
 num_paths = 2
 num_nodes_per_path = 2
+node_content = ' TESTING TESTING TESTING DCOS NODE being TESTED DCOS NODE BEing TESTED Check node contents'
 
 print(zk_hosts)
 
-def zk_write_test(zk_root_path,zk_node_name,num_paths,num_nodes_per_path):
+def zk_write_test(zk_root_path,zk_node_name,num_paths,num_nodes_per_path,node_content):
     start_time=datetime.datetime.now()
     for i in range(0,num_paths):
         full_zk_path = "{}/{}-{}".format(zk_root_path,'path',str(i))
         zk.ensure_path(full_zk_path)
         for j in range(0,num_nodes_per_path):
-            zk.create("{}/{}-{}".format(full_zk_path,zk_node_name,j), b"ITESTING !")
+            zk.create("{}/{}-{}".format(full_zk_path,zk_node_name,j), str.encode(node_content))
     end_time=datetime.datetime.now()
     delta_time=end_time - start_time
     print("Time elapsed = " + str(delta_time) +" seconds. \n")
@@ -38,15 +39,22 @@ def zk_checks(zk_host,zk_port,verb):
 
 if __name__ == "__main__":
 
+
+    ## RESET The STATISTICS - ONE TIME
+    for zk_host in zk_hosts.split(','):
+        print("Resetting stats on ZK Host # " + zk_host)
+        zk_ok = zk_checks(zk_host, zk_port,'srst')
+
+
     # Work on ZK WRITES
     count=0
+    print("Number of ZK Paths to write = "+ str(num_paths))
+    print("Number of ZK Nodes in each path = "+ str(num_nodes_per_path))
     for zk_host in zk_hosts.split(','):
         print("Working on ZK Host # "+ str(count) +" , " + zk_host)
-        print("Number of ZK Paths = "+ str(num_paths))
-        print("Number of ZK Nodes in each path = "+ str(num_nodes_per_path))
         zk = KazooClient(hosts=zk_host)
         zk.start()
-        zk_write_test('{}-{}'.format(zk_root_path,count),zk_node_name,num_paths,num_nodes_per_path)
+        zk_write_test('{}-{}'.format(zk_root_path,count),zk_node_name,num_paths,num_nodes_per_path,node_content)
         count=count + 1
         zk.stop()
 
